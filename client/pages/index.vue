785fb4ae1ea005f8f6f71959f87a11fd356493b2
Léo Nicaisse <contact@leonicaisse.fr>
2021-08-16 18:48:26 +0200

    Added project description & installation guide home homepage

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -7,7 +7,9 @@
       <hr>
       <v-card-text>
         <section id="goals" class="my-5">
-          <h2 class="mb-2">Objectif :</h2>
+          <h2 class="mb-2">
+            Objectif :
+          </h2>
           <p>
             Créer une application permettant l'administration d'une bibliothèque
             de musiques. Cette application se décompose en deux pages :
@@ -27,7 +29,9 @@
           </ul>
         </section>
         <section id="bonus" class="my-5">
-          <h2 class="mb-2">Bonus</h2>
+          <h2 class="mb-2">
+            Bonus
+          </h2>
           <ul>
             <li>L'application utilise Vuetify</li>
             <li>
@@ -42,18 +46,67 @@
           </ul>
         </section>
         <section id="technologies" class="my-5">
-          <h2 class="mb-2">Technologies utilisées</h2>
+          <h2 class="mb-2">
+            Technologies utilisées
+          </h2>
           <ul>
             <li>API/Back: Api Platform (Symfony, MySQL)</li>
-            <li>Front: Vue.js / Vuetify</li>
+            <li>Front: Vue.js (Nuxt) / Vuetify</li>
           </ul>
         </section>
         <section id="installation" class="my-5">
-          <h2 class="mb-2">Installation</h2>
-          <code>A REDIGER</code>
+          <h2 class="mb-2">
+            Installation
+          </h2>
+
+          <section id="client" class="my-5">
+            <h3>Client</h3>
+
+            <div class="my-3">
+              <h4>Installation :</h4>
+              <code>cd client && npm install</code>
+            </div>
+            <div class="my-3">
+              <h4>Lancement de l'application :</h4>
+              <code>npm run dev</code>
+            </div>
+            <div class="my-3">
+              <h4>Pour lancer les tests :</h4>
+              <code>npm run test</code>
+            </div>
+          </section>
+
+          <section id="api" class="my-5">
+            <h3>API</h3>
+            <p>L'API nécessite une version supérieure ou égale à PHP 8.0</p>
+            <div class="my-3">
+              <h4>Installation :</h4>
+              <p class="mt-3 mb-0">
+                Tout d'abord, s'il n'est pas installé sur votre système,
+                installez Symfony CLI
+              </p>
+              <code>curl -sS https://get.symfony.com/cli/installer | bash</code>
+              <p class="mt-3 mb-0">Puis installez les dépendences de l'application :</p>
+              <code>cd api && composer install</code>
+              <p class="mt-3 mb-0">
+                Ouvrez le fichier .env et configurez la connexion à la base de
+                données, puis créez cette base grâce aux commandes suivantes :
+              </p>
+              <code>php bin/console doctrine:database:create</code><br>
+              <code>php bin/console doctrine:schema:create</code>
+              <p class="mt-3 mb-0">Chargez ensuite les fixtures (données pré-enregistrées)</p>
+              <code>php bin/console doctrine:fixtures:load</code>
+            </div>
+            <div class="my-3">
+              <h4>Lancement de l'API</h4>
+              <code>symfony serve</code>
+            </div>
+          </section>
         </section>
         <section id="credits" class="my-5">
-          <h2 class="mb-2">Réalisé par</h2>
+          <h2 class="mb-2">
+            Réalisé par
+          </h2>
           <p><a href="https://leonicaisse.fr">Léo Nicaisse</a>, 2021</p>
         </section>
       </v-card-text>
